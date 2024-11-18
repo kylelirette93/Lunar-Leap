8d703dbae828da86b66b407524b098cf7db4641c
Kyle Lirette <162719550+kylelirette93@users.noreply.github.com>
2024-11-18 13:39:05 -0400

    Fixed camera culling for second level

--- Assets/Scripts/UI and Systems/CameraController.cs
+++ Assets/Scripts/UI and Systems/CameraController.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {    
-    public GameObject player;  
+    public GameObject player;
     private Vector3 offset;
+    
 
    
     void Start()
     {
         offset = transform.position - player.transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Backwall"))
+        {
+            Debug.Log("Triggering camera culling.");
+            // Exclude the Backwall layer from the camera's culling mask
+            int backwallLayer = LayerMask.NameToLayer("Backwall");
+            if (backwallLayer != -1) // Check if the layer exists
+            {
+                Camera.main.cullingMask &= ~(1 << backwallLayer);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Backwall"))
+        {
+            // Include the Backwall layer back into the camera's culling mask
+            int backwallLayer = LayerMask.NameToLayer("Backwall");
+            if (backwallLayer != -1)
+            {
+                Camera.main.cullingMask |= (1 << backwallLayer);
+            }
+        }
     }
 
     void LateUpdate()
